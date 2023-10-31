713a7aa906782a1cb1e2e455c37d5e56c2dc26c2
Wriddhi Hazra <wriddhihazra@gmail.com>
2023-10-31 12:50:16 +0530

    Added jsonLD for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 // "use client";
-import { Header } from "@/components/Header";
 import "./globals.css";
+import { Header } from "@/components/Header";
+import Head from "next/head";
+import { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import localFont from "next/font/local";
 import Footer from "@/components/Footer";
 
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const machina = localFont({
   src: [
@@ -30,22 +32,52 @@ const machina = localFont({
 
 const outfit = Outfit({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Wriddhi Hazra",
   description: "Software Engineer based in India.",
+  openGraph: {
+    title: "Wriddhi Hazra",
+    description: "Software Engineer based in India.",
+  },
 };
 
-const Head = () => (
-  <head>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="manifest" href="/site.webmanifest" />
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-    <link rel="canonical" href="https://www.wriddhi.com/" />
-    <meta name="msapplication-TileColor" content="#000000" />
-    <meta name="theme-color" content="#000000" />
-  </head>
+const JSONLD = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Wriddhi Hazra",
+  alternateName: "Wriddhi",
+  url: "https://www.wriddhi.com/",
+  image: "https://www.wriddhi.com/me-website.png",
+  sameAs: ["https://www.linkedin.com/in/wriddhi-hazra/"],
+};
+
+const CustomHead = () => (
+  <>
+    <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+      <link rel="canonical" href="https://www.wriddhi.com/" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#000000" />
+    </Head>
+  </>
 );
 
 export default function RootLayout({
@@ -55,7 +87,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en-US">
-      <Head />
+      <CustomHead />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(JSONLD) }}
+      />
       <body
         className={`${outfit.className} ${machina.variable} overflow-x-hidden relative bg-black min-h-screen h-full w-screen flex flex-col items-center`}
       >
